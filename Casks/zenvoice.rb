f925b45b4bfdb48ef09e039ed0b261e48abc8a05
Yash Chaudhary <imYash.Chaudhary973@gmail.com>
2026-08-27 03:38:58 +0530

    chore(release): update Homebrew cask for v0.4.0 DMG

--- Casks/zenvoice.rb
+++ Casks/zenvoice.rb
@@ -1,11 +1,11 @@
 cask "zenvoice" do
-  version "0.0.0-placeholder"
-  sha256 :no_check
+  version "0.4.0"
+  sha256 "33c5399927888386e7e755920a7d96af1e990c511c5e9b39c35681c048e30b94"
 
-  url "https://github.com/zenvoice/ZenVoice/releases/download/v#{version}/ZenVoice-distribution.zip"
+  url "https://github.com/imYashChaudhary973/ZenVoice/releases/download/v#{version}/ZenVoice.dmg"
   name "ZenVoice"
-  desc "Free, open-source, privacy-first macOS dictation"
-  homepage "https://github.com/zenvoice/ZenVoice"
+  desc "Privacy-first macOS speech capture, transcription, and lecture summaries"
+  homepage "https://github.com/imYashChaudhary973/ZenVoice"
 
   auto_updates true
 
@@ -14,6 +14,6 @@ cask "zenvoice" do
   zap trash: [
     "~/Library/Application Support/ZenVoice",
     "~/Library/Caches/ZenVoice",
-    "~/Library/Preferences/app.zenvoice.ZenVoice.plist",
+    "~/Library/Preferences/dev.yashchaudhary.ZenVoice.plist",
   ]
 end
